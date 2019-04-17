a2f35b7a6e97e02153c31f7551878b78436611a7
Michael Trang <michael.trang@hotmail.com>
2019-04-17 22:59:37 +1000

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div>
+      <img src="../assets/robot-home.png" alt="Robot" aria-hidden="true" class="robot" />
+    </div>
+    <div class="get-started"><a href>Get started</a> building a first robot!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 300px;
+}
+</style>
